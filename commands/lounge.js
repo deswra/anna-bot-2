5cef633954ecd2d78d5190929d8b863db067e81f
Des <dtennos@yahoo.com>
2019-02-22 21:58:49 +0700

    Complete lounge command

--- commands/lounge.js
+++ commands/lounge.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch');
 const moment = require('moment');
 const princess = require('../functions/princess');
 
@@ -10,14 +9,21 @@ module.exports.run = async (anna, message, args) => {
     const searchResult = await searchResults[0];
     if (searchResult !== undefined) {
       const loungeData = await princess.getLoungeData(searchResult.id);
-      const loungeHistory = await princess.getLoungeHistory(searchResult.id);
+      let loungeHistory = await princess.getLoungeHistory(searchResult.id);
+      loungeHistory = loungeHistory.sort((a, b) => a.rank - b.rank).slice(0, 5);
+      let loungeHistoryRes = '';
+      loungeHistory.forEach(event => {
+        loungeHistoryRes += `${event.rank} - ${event.eventName.includes('～')?event.eventName.split('～')[1]:event.eventName}\n`
+      });
+      loungeHistoryRes = loungeHistoryRes.slice(0, -1);
       const response = new Discord.RichEmbed()
         .setColor('#7e6ca8')
         .setAuthor(loungeData.name)
-        .setTitle(`Lounge was founded on ${moment(loungeData.createTime).format('MMMM Do YYYY')}.`)
-        .setDescription(loungeData.comment);
+        .setTitle(`Lounge was founded on ${moment(loungeData.createTime).format('MMMM Do YYYY')} by ${loungeData.masterName}.`)
+        .setDescription(loungeData.comment)
+        .addField('Highest ranked events', loungeHistoryRes, false);
+      return message.channel.send(response);
     }
-    return message.channel.send(response);
   } else {
     return message.channel.send(`Anna needs a lounge name, ${message.author}P-san...`);
   }
